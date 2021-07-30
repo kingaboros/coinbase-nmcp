cd3c53929ba75c2d6ef66bb04c4a3f0540b34199
Kinga Boros <kinga.boros20@gmail.com>
2021-07-30 15:36:29 +0300

    added project images and deleted some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Homepage, About } from './components';
-
-import NavBar from './Navbar/Navbar';
-
-import SignUp from './components/Auth/Signup';
-import LogIn from './components/Auth/LogIn';
-import ForgotPassword from './components/Auth/ForgotPassword';
-import MyProfile from './components/Auth/MyProfile';
 
 import './App.scss';
 
 const App = () => {
-  return (
-    <Router>
-      <div className="App-layer">
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/about" component={About} />
-          <Route path="/myprofile" component={MyProfile} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={LogIn} />
-          <Route path="/forgot-password" component={ForgotPassword} />
-        </Switch>
-      </div>
-    </Router>
-  );
+  return <div>hey</div>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
-//Redux
-import { Provider } from 'react-redux';
-
-import { store } from './store/store';
-
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
