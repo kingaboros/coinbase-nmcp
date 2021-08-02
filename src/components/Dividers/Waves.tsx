1d2d68889b8981fc695ed117d278a23e16110bb9
Kinga Boros <kinga.boros20@gmail.com>
2021-08-02 16:00:48 +0300

    added waves component

---
+++ src/components/Dividers/Waves.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as classes from './Waves.module.scss';
+
+const Waves = () => {
+  return (
+    <div className={classes.wavesWrapper}>
+      <div></div>
+    </div>
+  );
+};
+
+export default Waves;
