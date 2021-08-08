8b9834a3702410dea8cfa2f2dac990dc2fea824f
Kinga Boros <kinga.boros20@gmail.com>
2021-08-08 15:14:38 +0300

    making rates table responsive

--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Header from '../components/Header/Header';
-import Wrapper from '../components/Wrapper/Wrapper';
 
 import Paragraphs from '../components/Paragraphs/Paragraphs';
 import RatesTable from '../components/RatesTable/RatesTable';
